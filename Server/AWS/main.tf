5010c67eaeb94c0e2dc960c3459de02e8ab78279
Lemo-nade-room <lemo.nade.engineer.room@gmail.com>
2025-06-07 23:53:49 +0900

    docker build & pushのCode Build Project作成

--- Server/AWS/main.tf
+++ Server/AWS/main.tf
@@ -25,6 +25,41 @@ resource "aws_ecr_repository" "query_server_function_repository" {
 # ================================
 # Deploy Pipeline
 # ================================
+resource "aws_codebuild_project" "docker_build_and_push" {
+  name          = "docker_build_and_push"
+  description   = "Docker ImageをBuildし、ECRへプッシュするCodeBuildプロジェクトです。使用時にはREPOSITORY_URL, DOCKER_FILE_PATH, TAGの環境変数のOverrideが必要です"
+  build_timeout = 15
+  service_role  = aws_iam_role.docker_build_role.arn
+
+  artifacts {
+    type = "CODEPIPELINE"
+  }
+
+  environment {
+    compute_type                = "BUILD_GENERAL1_LARGE"
+    image                       = "aws/codebuild/amazonlinux-aarch64-standard:3.0-25.03.03"
+    type                        = "ARM_CONTAINER"
+    image_pull_credentials_type = "CODEBUILD"
+    privileged_mode             = true
+
+    environment_variable {
+      name  = "AWS_REGION"
+      value = var.region
+    }
+  }
+
+  logs_config {
+    cloudwatch_logs {
+      status = "ENABLED"
+    }
+  }
+
+  source {
+    type      = "CODEPIPELINE"
+    buildspec = "Server/AWS/docker_build_and_push_buildspec.yaml"
+  }
+}
+
 resource "aws_iam_role" "docker_build_role" {
   name = "docker_build_role"
   assume_role_policy = jsonencode({
