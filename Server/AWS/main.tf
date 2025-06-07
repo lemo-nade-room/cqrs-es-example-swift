b92b309ade1b7164189ac992fc56154466c80c06
Lemo-nade-room <lemo.nade.engineer.room@gmail.com>
2025-06-08 03:56:49 +0900

    ECR側にLambdaからのPull許可を追加
    最新のアクション実行メッセージ
    Failed to create stack. Current status: ROLLBACK_COMPLETE. First failed resource CommandServerFunction, Type:AWS::Lambda::Function, StatusReason:Resource handler returned message: "Lambda does not have permission to access the ECR image. Check the ECR permissions. (Service: Lambda, Status Code: 403)"

--- Server/AWS/main.tf
+++ Server/AWS/main.tf
@@ -21,6 +21,28 @@ resource "aws_ecr_repository" "query_server_function_repository" {
     scan_on_push = true
   }
 }
+data "aws_iam_policy_document" "lambda_pull" {
+  statement {
+    principals {
+      type = "Service"
+      identifiers = ["lambda.amazonaws.com"]
+    }
+
+    actions = [
+      "ecr:GetDownloadUrlForLayer",
+      "ecr:BatchGetImage",
+      "ecr:BatchCheckLayerAvailability",
+    ]
+  }
+}
+resource "aws_ecr_repository_policy" "command_server_function" {
+  repository = aws_ecr_repository.command_server_function_repository.name
+  policy     = data.aws_iam_policy_document.lambda_pull.json
+}
+resource "aws_ecr_repository_policy" "query_server_function" {
+  repository = aws_ecr_repository.query_server_function_repository.name
+  policy     = data.aws_iam_policy_document.lambda_pull.json
+}
 
 # ================================
 # Deploy Pipeline
@@ -237,8 +259,8 @@ resource "aws_iam_role_policy" "cloudformation_deploy_lambda" {
         Resource = "*"
       },
       {
-        Effect = "Allow"
-        Action = "iam:PassRole"
+        Effect   = "Allow"
+        Action   = "iam:PassRole"
         Resource = "*"
       }
     ]
