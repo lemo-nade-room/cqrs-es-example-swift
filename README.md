1051820199109bd37cb1377ed3654c89c2019202
Lemo-nade-room <lemo.nade.engineer.room@gmail.com>
2025-06-21 23:00:04 +0900

    Jaeger用の設定とサービスを追加

--- README.md
+++ README.md
@@ -3,8 +3,8 @@
 ## 開発環境
 
 ```shell
+docker compose pull
 docker compose up -d
 docker compose exec dev bash
-
-
+bash setup.sh
 ```
--- setup.sh
+++ setup.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 sudo chown -R claude:claude /claude/workspace/Server/.build
+cd /claude/workspace/Server && docker compose up -d
